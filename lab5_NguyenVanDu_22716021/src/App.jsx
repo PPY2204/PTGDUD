794c0fd0e42e1686f4a42183ec5dbd47b42ff686
PPY2204 <nguyenvandu222k4@gmail.com>
2025-03-19 23:22:31 +0700

    add lab5

---
+++ lab5_NguyenVanDu_22716021/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UseRef from "./api/UseRef";
+import API from "./api/API";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <UseRef />
+      <API />
+    </>
+  );
+}
+
+export default App;
