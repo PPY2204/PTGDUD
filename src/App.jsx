51749ca662e922f841a9b81fe435debc11472397
H51M14\Student <nguyenvandu222k4@gmail.com>
2025-04-23 15:59:57 +0700

    feat: hiển thị tổng  số sách

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,27 @@ function App() {
     .filter((book) => book.title.toLowerCase().includes(searchTerm.toLowerCase()))
     .filter((book) => !genreFilter || book.genre === genreFilter);
   return (
-    <>
+    <div className='p-4'>
       <AddBookForm onAdd={addBook} />
-      <input
-        type="text"
-        placeholder="Tìm kiếm theo tên"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <select value={genreFilter} onChange={(e) => setGenreFilter(e.target.value)}>
-        <option value="">Tất cả</option>
-        <option value="Văn học">Văn học</option>
-        <option value="Khoa học">Khoa học</option>
-        <option value="Công nghệ">Công nghệ</option>
-        <option value="Tâm lý">Tâm lý</option>
-      </select>
-
+      <div className='flex gap-10'>
+        <input
+          type="text"
+          placeholder="Tìm sách..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border p-2 rounded w-50 mb-2"
+        />
+        <select value={genreFilter} onChange={(e) => setGenreFilter(e.target.value)} className="border p-2 rounded w-50 mb-2">
+          <option value="">Tất cả</option>
+          <option value="Văn học">Văn học</option>
+          <option value="Khoa học">Khoa học</option>
+          <option value="Công nghệ">Công nghệ</option>
+          <option value="Tâm lý">Tâm lý</option>
+        </select>
+      </div>
+      <h2 className="text-lg font-semibold mb-2">Tổng số sách: {books.length}</h2>
       <BookList books={filteredBooks} onDelete={deleteBook} />
-    </>
+    </div>
   )
 }
 
