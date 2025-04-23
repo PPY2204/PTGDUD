5e80415e242113851ae4b4dea14436bd9498f57c
H51M14\Student <nguyenvandu222k4@gmail.com>
2025-04-23 15:32:56 +0700

    init: cau truc du an ReactJS

---
+++ src/components/BookList.jsx
@@ -0,0 +1,21 @@
+// src/components/BookList.jsx
+const books = [
+    { id: 1, title: "Dế Mèn Phiêu Lưu Ký", author: "Tô Hoài", genre: "Văn học", year: 1941 },
+    { id: 2, title: "Sapiens", author: "Yuval Harari", genre: "Khoa học", year: 2011 },
+];
+
+export default function BookList({ onDelete }) {
+    return (
+        <div>
+            <h2>Danh sách sách</h2>
+            <ul>
+                {books.map((book) => (
+                    <li key={book.id}>
+                        {book.title} - {book.author} - {book.genre} - {book.year}
+                        <button onClick={() => onDelete(book.id)}>Xoá</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
