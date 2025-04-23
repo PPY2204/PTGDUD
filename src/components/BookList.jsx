fa741548bcf48e0f7d85eb754c01ed2cff05c058
H51M14\Student <nguyenvandu222k4@gmail.com>
2025-04-23 16:15:53 +0700

    style: cải thiện giao diện

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,7 +2,7 @@ import BookItem from "./BookItem";
 
 export default function BookList({ books, onDelete }) {
     return (
-        <ul className="space-y-2">
+        <ul className="space-y-2 ">
             {books.map((book) => (
                 <BookItem key={book.id} book={book} onDelete={onDelete} />
             ))}
